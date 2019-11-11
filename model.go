aac15307025afeac5d79b13afacbc591a62c8f6e
James Schneider <jschneider98@gmail.com>
2019-11-11 10:13:05 -0800

    Change spacing

--- model.go
+++ model.go
@@ -61,10 +61,9 @@ func (m *Model) GetInsertQuery() string {
 		}
 	}
 
-	colList = strings.Join(dbCols, ",")
-	colList = strings.ReplaceAll(colList, ",", ",\n")
+	colList = strings.Join(dbCols, ",\n\t")
 
-	query := fmt.Sprintf("\t\tINSERT INTO\n\t\t%s (%s)\n\t\tVALUES (%s)\n\t\tRETURNING id\n", m.FullTableName, colList, strings.Join(placeHolders, ","))
+	query := fmt.Sprintf("INSERT INTO\n%s (%s)\nVALUES (%s)\nRETURNING id\n", m.FullTableName, colList, strings.Join(placeHolders, ","))
 
 	return query
 }
